5ce28c3efa354855c73571665f45f6185e83a176
Praises003 <praiseabiodun15@gmail.com>
2025-06-25 15:49:19 +0100

    update

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "../generated/prisma"
+import { PrismaClient } from '@prisma/client';
 import dotenv from 'dotenv';
 import { hash, compare } from "bcryptjs";
 import { sign, verify } from "jsonwebtoken";
